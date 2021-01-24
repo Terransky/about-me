4cd0f3be6a093902eb1ef10d664fe25232169d1f
Terransky <nodeathking@gmail.com>
2021-01-23 19:15:20 -0800

    added CSS elements to pretty up page.

--- js/app.js
+++ js/app.js
@@ -11,16 +11,13 @@ function functionOne(){
 var responseOne = prompt("Do you think I'm an earthling?").toLowerCase();
 
 if(responseOne === 'yes' || responseOne === 'y' ){
-  // console.log("Good, good, exactly what I want you to think.");
   alert("Good, good, exactly what I want you to think.");
   score++;
 
 } else if(responseOne === 'no' || responseOne === 'n'){
-    // console.log("Of course I am, how dare you. I'll remember this when we conquer this planet.");
     alert("Of course I am, how dare you. I'll remember this when we conquer this planet.");
 
 } else {
-  // console.log("Please answer yes or no.");
   alert("Please answer yes or no.");
 }
 }
@@ -32,16 +29,13 @@ function functionTwo(){
   var responseTwo = prompt("Do I play video games?").toLowerCase();
 
 if(responseTwo === 'yes' || responseTwo === 'y' ){
-  // console.log("Indeed, I'm an avid gamer and currently looking forward to Monster Hunter Rise.");
   alert("Indeed, I'm an avid gamer and currently looking forward to Monster Hunter Rise.");
   score++;
 
 } else if(responseTwo === 'no' || responseTwo === 'n'){
-    // console.log("Really? Don't I look like I do though?");
     alert("Really? Don't I look like I do though?");
 
 } else {
-  // console.log("Please answer yes or no.");
   alert("Please answer yes or no.");
 }
 }
@@ -52,16 +46,13 @@ function functionThree(){
 var responseThree = prompt("Do I drink alcohol?").toLowerCase();
 
 if(responseThree === 'yes' || responseThree === 'y' ){
-  // console.log("Most nights since I have trouble sleeping.");
   alert("Most nights since I have trouble sleeping.");
   score++;
 
 } else if(responseThree === 'no' || responseThree === 'n'){
-    // console.log("I'm actually a big fan of gin");
     alert("I'm actually a big fan of gin.");
 
 } else {
-  // console.log("Please answer yes or no.");
   alert("Please answer yes or no.");
 }
 }
@@ -73,16 +64,13 @@ function functionFour(){
 var responseFour = prompt("Am I an old man?").toLowerCase();
 
 if(responseFour === 'yes' || responseFour === 'y' ){
-  // console.log("I think so too.");
   alert("I think so too.");
   score++;
 
 } else if(responseFour === 'no' || responseFour === 'n'){
-    // console.log("I sure feel like one.");
     alert("I sure feel like one.");
 
 } else {
-  // console.log("Please answer yes or no.");
   alert("Please answer yes or no.");
 }
 }
@@ -94,16 +82,13 @@ function functionFive(){
   var responseFive = prompt("Have I ever rode a unicycle.").toLowerCase();
 
 if(responseFive === 'yes' || responseFive === 'y' ){
-  // console.log("I was hoping to develop a sense of balance and strong core.");
   alert("I was hoping to develop a sense of balance and strong core.");
   score++;
 
 } else if(responseFive === 'no' || responseFive === 'n'){
-   // console.log("Surprisingly true, I have the scars to prove it.");
   alert("Surprisingly true, I have the scars to prove it.");
 
 } else {
-  // console.log("Please answer yes or no.");
   alert("Please answer yes or no.");
 }
 }
